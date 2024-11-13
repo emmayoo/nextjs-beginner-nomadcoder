b81ae88a7707432a9607a5be7015dcaa68812476
emmayoo <emmayoo9663@gmail.com>
2024-11-13 18:17:34 +0900

    ADD: loading 파일

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,14 @@ export const metadata = {
   title: "Home",
 };
 
-export default function Home() {
-  return <h1>Hello!</h1>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  return fetch(URL).then((res) => res.json());
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+  return <h1>Hello!{JSON.stringify(movies)}</h1>;
 }
