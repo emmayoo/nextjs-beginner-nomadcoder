668ebed2193f895c4de121a319512d164896a8aa
emmayoo <emmayoo9663@gmail.com>
2024-11-13 17:53:52 +0900

    FIX: hydrate mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navigation />
-      <body>{children}</body>
+      <body>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
