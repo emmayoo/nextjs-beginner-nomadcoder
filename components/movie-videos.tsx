2e6179bcaf88b177cfa5babee7513582527f6b7b
emmayoo <emmayoo9663@gmail.com>
2024-11-13 20:12:25 +0900

    FIX: remove Error

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,8 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  throw new Error("something wrong :(");
-  // const response = await fetch(`${API_URL}/${id}/videos`);
-  // return response.json();
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
