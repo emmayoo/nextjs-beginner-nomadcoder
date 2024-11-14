caf57cdd9a8066b139ce416c3769199e1c6bbe9f
emmayoo <emmayoo9663@gmail.com>
2024-11-14 14:08:29 +0900

    ADD: dynamic metadata

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../components/movie-info.tsx";
+import MovieInfo, { getMovie } from "../../../components/movie-info.tsx";
 import MovieVideos from "../../../components/movie-videos.tsx";
 
-export default async function MovieDetail({
-  params,
-}: {
+interface IParams {
   params: Promise<{ id: string }>;
-}) {
+}
+
+export async function generateMetadata({ params }: IParams) {
+  const id = (await params).id;
+
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params }: IParams) {
   const id = (await params).id;
 
   return (
